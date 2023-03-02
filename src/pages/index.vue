68276a647b8d2893d2281438af19537181773c0b
Tom <athomas6248@gmail.com>
2023-03-02 17:46:14 -0500

    Basic HTML Strutcture

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,100 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <div class="container">
+    <header>
+      <img src="">
+      <nav>
+        <ul>
+          <li>Writings</li>
+          <li>Projects</li>
+          <li>Resume</li>
+          <li>Contacts</li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <section>
+        <h1>
+          <span>Yes,I’m that</span>
+          <span>Luke</span>
+          <span>Weston.</span>
+
+        </h1>
+        <h2>I make things for the internet.</h2>
+        
+        <p>More accurately, I get other people to do it because I’m a really busy guy and so are you!</p>
+
+        <button>Let's chat</button>
+      </section>
+
+      <section class="section-one">
+        <h3>Featured Projects</h3>
+        <article>
+        <div class="featured-image"></div>
+        <div class="description"></div>
+        </article>
+      
+        <article>
+        <div class="featured-image"></div>
+        <div class="description"></div>
+        </article>
+      </section>
+
+      
+      
+      <section class="section-two">
+        <h3>Featured Writings</h3>
+       <div>
+          <article>
+          <figure>
+            <img src="https://placehold.jp/800x600.png"
+            alt="Elephant at sunset">
+            <figcaption>An elephant at sunset</figcaption>
+          </figure>
+          </article>
+      
+          <article>
+          <figure>
+            <img src="https://placehold.jp/800x600.png"
+            alt="Elephant at sunset">
+            <figcaption>An elephant at sunset</figcaption>
+          </figure>
+          </article>
+
+          <article>
+          <figure>
+            <img src="https://placehold.jp/800x600.png"
+            alt="Elephant at sunset">
+            <figcaption>An elephant at sunset</figcaption>
+          </figure>
+          </article>
+        
+          <article>
+          <figure>
+            <img src="https://placehold.jp/800x600.png"
+            alt="Elephant at sunset">
+            <figcaption>An elephant at sunset</figcaption>
+          </figure>
+          </article>
+
+          <article>
+          <figure>
+            <img src="https://placehold.jp/800x600.png"
+            alt="Elephant at sunset">
+            <figcaption>An elephant at sunset</figcaption>
+          </figure>
+          </article>
+        
+          <article>
+          <figure>
+            <img src="https://placehold.jp/800x600.png"
+            alt="Elephant at sunset">
+            <figcaption>An elephant at sunset</figcaption>
+          </figure>
+          </article>
+        </div>
+        </section>
+      </main>
+    </div>
 </template>
 
 <script lang="ts" setup>
